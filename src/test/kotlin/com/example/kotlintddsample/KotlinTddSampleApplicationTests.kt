8a07e01a59f35a0c6a38ecb376a36c4bf0b0b3ca
ydj515 <ydj515@hanmail.net>
2024-11-22 23:00:37 +0900

    init commit

---
+++ src/test/kotlin/com/example/kotlintddsample/KotlinTddSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlintddsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinTddSampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
