5e4d496fe4d6d99bd86b0b1e6bdae7d6a457d306
ydj515 <ydj515@hanmail.net>
2024-11-23 12:47:54 +0900

    feat: product Controller test 추가

---
+++ src/test/kotlin/com/example/kotlintddsample/ProductControllerTest.kt
@@ -0,0 +1,35 @@
+package com.example.kotlintddsample
+
+import com.ninjasquad.springmockk.MockkBean
+import io.kotest.core.spec.style.DescribeSpec
+import io.mockk.every
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+
+@WebMvcTest(ProductController::class)
+class ProductControllerTest(
+    @Autowired private val mockMvc: MockMvc,
+    @MockkBean private val productService: ProductService
+) : DescribeSpec({
+
+    beforeEach {
+        every { productService.getProducts() } returns listOf(
+            ProductDto(1, "Americano", 3000),
+            ProductDto(2, "Latte", 4000)
+        )
+    }
+
+    describe("GET /api/coffees") {
+        it("should return list of coffee menus") {
+            mockMvc.get("/api/product")
+                .andExpect {
+                    status { isOk() }
+                    jsonPath("$[0].menuId") { value(1) }
+                    jsonPath("$[0].name") { value("Americano") }
+                    jsonPath("$[0].price") { value(3000) }
+                }
+        }
+    }
+})
