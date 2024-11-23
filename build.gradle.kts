4d43b1246806896ce38bf6f647b19f332883269c
dj.yoo <dj.yoo@promptech.co.kr>
2024-11-23 12:39:42 +0900

    feat: jpa 추가 및 springboot 버전 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.4.0"
+    id("org.springframework.boot") version "3.3.5"
     id("io.spring.dependency-management") version "1.1.6"
+    kotlin("plugin.jpa") version "1.9.25"
 }
 
 group = "com.example"
