d2508b1419cf1d0ac8653da93b32904561e67e51
ydj515 <ydj515@hanmail.net>
2024-11-24 18:38:16 +0900

    test: product repository test 추가

---
+++ src/test/kotlin/com/example/kotlintddsample/ProductRepositoryTest.kt
@@ -0,0 +1,35 @@
+package com.example.kotlintddsample
+
+import com.example.kotlintddsample.product.Product
+import com.example.kotlintddsample.product.ProductRepository
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+
+@DataJpaTest
+class ProductRepositoryTest(
+    @Autowired val productRepository: ProductRepository
+) : DescribeSpec({
+
+    describe("ProductRepository") {
+        context("findAll()을 호출하면") {
+            it("데이터베이스에서 product list를 리턴한다.") {
+                // given
+                val coffee1 = Product(id = 1, name = "Americano", price = 3000)
+                val coffee2 = Product(id = 2, name = "Latte", price = 4000)
+                productRepository.saveAll(listOf(coffee1, coffee2))
+
+                // when
+                val result = productRepository.findAll()
+
+                // then
+                result shouldHaveSize 2
+                result[0].name shouldBe "Americano"
+                result[1].price shouldBe 4000
+            }
+        }
+
+    }
+})
